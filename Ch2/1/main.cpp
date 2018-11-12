f4a3390534e16759a8cec5e124f90a7ff53e4eb0
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-11-12 14:13:41 +0100

    Added solution to problem 1 from chapter 2.

---
+++ Ch2/1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+  std::string name = "Holmes";
+  std::string address = "221B Baker Street";
+  std::cout << name << ", " <<  address << std::endl;
+  return 0;
+}
