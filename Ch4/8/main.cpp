d0f649df3d6bd16c8f6cc284baac740d62786bcb
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-12-06 15:43:44 +0100

    Added solution to problem 8 of chapter 4.

---
+++ Ch4/8/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+struct pizzaInfo{
+  std::string manufacturerName = "";
+  double diameter = 0.0;
+  double weight = 0.0;
+};
+
+int main() {
+  using namespace std;
+  // Again, no validation is necessary.
+
+  pizzaInfo *newPizza = new pizzaInfo();
+
+  cout << "Input pizza diameter.\n>";
+  cin >> newPizza->diameter;
+  cout << "Input pizza manufacturer name.\n>";
+  cin >> newPizza->manufacturerName;
+  cout << "Input pizza weight.\n>";
+  cin >> newPizza->weight;
+
+  cout  << "Info you provided is as follows. Pizza manufacturer name: "
+        << newPizza->manufacturerName << ", pizza weight: "
+        << newPizza->weight << ", pizza diameter: "
+        << newPizza->diameter << ".\n";
+
+  return 0;
+}
