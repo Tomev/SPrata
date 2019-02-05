5fa0c017dae9143d4de290baf68cd873e8d6a320
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-02-05 12:12:24 +0100

    Added refactor to problem 7.

--- Ch5/7/main.cpp
+++ Ch5/7/main.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
 #include <string>
 
-struct car{
+struct car {
   std::string name = "";
   int prodYear = -1;
 };
-
-void printCar(car c)
-{
+void printCar(car c) {
   std::cout << c.prodYear << " " << c.name << std::endl;
 }
-
 int main() {
-
   int numberOfCars = 0;
   std::cout << "Input number of cars you want to input.\n>";
   std::cin >> numberOfCars;
-
   std::cin.clear();
   std::cin.get();
-
-  car * cars = new car[numberOfCars];
-
-  for(int i = 0; i < numberOfCars; ++i)
-  {
+  car *cars = new car[numberOfCars];
+  for(int i = 0; i < numberOfCars; ++i) {
     std::cout << "Car #" << i + 1 << ":\nInput mark: ";
     std::getline(std::cin, cars[i].name);
     std::cout << "Input production year: ";
@@ -31,10 +23,8 @@ int main() {
     std::cin.clear();
     std::cin.get();
   }
-
   for(int i = 0; i < numberOfCars; ++i)
     printCar(*(cars + i));
-
   return 0;
 }
 
