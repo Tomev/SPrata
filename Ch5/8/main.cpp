0f31f235a70753e9758b17aa5fbcea9f1ad5205b
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-02-05 12:23:27 +0100

    Added solution to problem 8.

---
+++ Ch5/8/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+
+const int wordSize = 50;
+int main() {
+
+  int wordsNum = 0;
+
+  std::cout << "Input words. Type finish to exit.\n";
+  char word[wordSize] = "";
+
+  std::cin >> word;
+
+  while(std::strcmp(word, "finish"))
+  {
+    ++wordsNum;
+    std::cin >> word;
+  }
+
+  std::cout << "Number of input words " << wordsNum << ".";
+
+  return 0;
+}
