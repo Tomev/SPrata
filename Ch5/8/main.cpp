c3ba3ee3cfa6f81efba26882bd2ddf405ff4206c
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-02-05 12:24:26 +0100

    Added refactor to problem 8.

--- Ch5/8/main.cpp
+++ Ch5/8/main.cpp
@@ -3,21 +3,14 @@
 
 const int wordSize = 50;
 int main() {
-
   int wordsNum = 0;
-
   std::cout << "Input words. Type finish to exit.\n";
   char word[wordSize] = "";
-
   std::cin >> word;
-
-  while(std::strcmp(word, "finish"))
-  {
+  while(std::strcmp(word, "finish")) {
     ++wordsNum;
     std::cin >> word;
   }
-
   std::cout << "Number of input words " << wordsNum << ".";
-
   return 0;
 }
